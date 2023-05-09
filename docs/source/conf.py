e479f586b3c935d4eb4127be1634331bafbac800
Fredrik Boulund <fredrik.boulund@ki.se>
2023-05-09 13:51:06 +0200

    Bump version to 0.7.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,9 +56,9 @@ author = 'Fredrik Boulund'
 # built documents.
 #
 # The short X.Y version.
-version = '0.6.1'
+version = '0.7.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.6.1-dev'
+release = '0.7.0-dev'
 
 # reStructuredText prolog contains a string of reStructuredText that will be
 # included at the beginning of every source file that is read.
